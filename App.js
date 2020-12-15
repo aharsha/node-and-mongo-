91e3060c57f1163a48c35781ba026daf97dd1a7f
aharsha <alleni.harsha@gmail.com>
2020-12-15 16:53:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import A from './Test'
+import ProductList from './ProductList'
+function App() {
+  return (
+    <div>
+      <A/>
+    <ProductList/>
+    
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ ProductList.js
@@ -0,0 +1,22 @@
+import React , {Component } from 'react';
+class ProductList extends React.Component
+{
+    render()
+    {
+        return(
+        <ul>
+            <li>
+                Dell
+            </li>
+            <li>
+                Lenovo
+            </li>
+            <li>
+                Hp
+            </li>
+        </ul>
+            );
+    }
+}
+
+export default ProductList;
